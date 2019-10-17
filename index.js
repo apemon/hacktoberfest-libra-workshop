0f4d99284c05df00d78f72d5184060e9a20d2e5f
Parin Chiamananthapong <apemon642002@hotmail.com>
2019-10-18 00:57:34 +0700

    hello world :tada:

---
+++ index.js
@@ -0,0 +1,34 @@
+const { LibraWallet, LibraClient, LibraNetwork, KeyPair } = require("kulap-libra");
+
+const client = new LibraClient({network: LibraNetwork.Testnet});
+
+// create wallet
+const createWallet = async() => {
+
+}
+
+// query balance
+const getBalance = async() => {
+
+}
+
+// mint
+const mint = async(address, amount) => {
+
+}
+
+// transfer
+const transfer = async(account, recipient, amount) => {
+
+}
+
+// get transaction (extra)
+const getTransactions = async(address) => {
+    
+}
+
+(async () => {
+    // do something here
+    console.log('hello hacktoberfest')
+})()
+
